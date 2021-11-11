143b9fd40ed4b5bf12970c3a124d4d10f48ed911
Yu4iy <yurimohyliuk@gmail.com>
2021-11-11 15:52:28 +0100

    setup project

---
+++ main.js
@@ -0,0 +1,27 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data di Vue.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue({
+	el: '.app',
+	data:{
+		greater:'HELLO WORLD',
+		img:'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/03/03e2db963b50366638563aff851ce5bcc6ffbc4e_full.jpg',
+ 		titleClass:'red'
+
+	},
+	methods:{
+		changeColor(){
+			this.titleClass = this.titleClass === 'red'? 'blue':'red';
+		}
+	}
+})
+
+
+
+
+
+
+
+
+
