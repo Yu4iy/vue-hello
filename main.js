cd3b10705a9df85679896cfc25ca12fe66df0ab0
Yu4iy <yurimohyliuk@gmail.com>
2021-11-11 21:06:35 +0100

    add joke

--- main.js
+++ main.js
@@ -7,13 +7,21 @@ const app = new Vue({
 	data:{
 		greater:'HELLO WORLD',
 		img:'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/03/03e2db963b50366638563aff851ce5bcc6ffbc4e_full.jpg',
- 		titleClass:'red'
+ 		titleClass:'red',
+		count:0
 
 	},
 	methods:{
 		changeColor(){
 			this.titleClass = this.titleClass === 'red'? 'blue':'red';
-		}
+			this.count++
+			if(this.count === 4){
+				this.greater = 'SMETTILA CLICCARE, e fai qualcosa di Utile 0_0'
+			}
+			
+		},
+
+	
 	}
 })
 
